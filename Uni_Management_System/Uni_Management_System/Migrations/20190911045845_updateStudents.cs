eb2433b07d03b87005cffa0fcd73c34d677226b5
Aasish Adhikari <gemini.aasis@gmail.com>
2019-09-25 13:53:56 +0930

    Week 8 Task 3 - Migrating to 3 tier architecture
    Step 1: Created both DAL and BAL class library

---
+++ Uni_Management_System/Uni_Management_System/Migrations/20190911045845_updateStudents.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Uni_Management_System.Migrations
+{
+    public partial class updateStudents : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "dateOfBirth",
+                table: "Students",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "email",
+                table: "Students",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "dateOfBirth",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "email",
+                table: "Students");
+        }
+    }
+}
---
+++ Uni_Management_System/Uni_Management_System/Migrations/20190911050109_updateStudentNo.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Uni_Management_System.Migrations
+{
+    public partial class updateStudentNo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "studentNo",
+                table: "Students",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "studentNo",
+                table: "Students");
+        }
+    }
+}
