1be319e20307247aef45c5bfd4cbacd8a9b9665b
Aasish Adhikari <gemini.aasis@gmail.com>
2019-10-13 13:44:30 +0930

    Week 11 Task 7 - Authentication - Scaffolding Identity with Applicationuser context

--- Uni_Management_System/Uni_Management_System/Areas/Identity/Pages/Account/ConfirmEmail.cshtml.cs
+++ Uni_Management_System/Uni_Management_System/Areas/Identity/Pages/Account/ConfirmEmail.cshtml.cs
@@ -6,15 +6,16 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Uni_Management_System.DAL;
 
 namespace Uni_Management_System.Areas.Identity.Pages.Account
 {
     [AllowAnonymous]
     public class ConfirmEmailModel : PageModel
     {
-        private readonly UserManager<IdentityUser> _userManager;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public ConfirmEmailModel(UserManager<IdentityUser> userManager)
+        public ConfirmEmailModel(UserManager<ApplicationUser> userManager)
         {
             _userManager = userManager;
         }
